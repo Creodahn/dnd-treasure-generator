4054058c4e9761358f8f8ba6361b15f5e746bff2
Justin Drew <justin.a.drew@gmail.com>
2019-02-25 11:42:01 -0700

    if treasure rule has item table attr, only use magic items as item type

--- app/components/hoard-rewards/component.js
+++ app/components/hoard-rewards/component.js
@@ -40,8 +40,8 @@ export default Component.extend({
         const { diceCount, dieType, itemTable, itemType, itemValue, multiplier } = calculation,
           { rolls, total } = this.diceBag.rollMultipleDice({ count: diceCount, dieType }),
           itemsToPickTotal = total * (multiplier || 1),
-          inflectedType = Inflector.inflector.pluralize(itemType).camelize(),
-          itemsToChooseFrom = itemTable ? rewardSource[inflectedType].filterBy('table', itemTable) : rewardSource[inflectedType].filterBy('value', itemValue),
+          inflectedType = itemType ? Inflector.inflector.pluralize(itemType).camelize() : '',
+          itemsToChooseFrom = itemTable ? rewardSource['magicItems'].filterBy('table', itemTable) : rewardSource[inflectedType].filterBy('value', itemValue),
           selectedItems = [],
           selectedItemNames = [];
         let countedResults = [],

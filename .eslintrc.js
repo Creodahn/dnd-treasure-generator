55ee3fd23c1494798f11ce9e223beef4f68f09fc
Justin Drew <justin.a.drew@gmail.com>
2019-05-18 15:44:52 +0000

    fix .eslintrc.js formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,133 +18,133 @@ module.exports = {
     browser: true
   },
   rules: {
-    "array-bracket-spacing": [
-      "error",
-      "never"
+    'array-bracket-spacing': [
+      'error',
+      'never'
     ],
-    "brace-style": "error",
-    "camelcase": [
-      "error",
+    'brace-style': 'error',
+    camelcase: [
+      'error',
       {
-        "properties": "never"
+        properties: 'never'
       }
     ],
-    "comma-style": "error",
-    "dot-notation": [
-      "error", {
-        "allowKeywords": true
+    'comma-style': 'error',
+    'dot-notation': [
+      'error', {
+        allowKeywords: true
       }
     ],
-    "indent": [
-      "error",
+    indent: [
+      'error',
       2, {
-        "SwitchCase": 1,
-        "VariableDeclarator": {
-          "var": 1,
-          "let": 1,
-          "const": 1
+        SwitchCase: 1,
+        VariableDeclarator: {
+          var: 1,
+          let: 1,
+          const: 1
         }
       }
     ],
-    "keyword-spacing": [
-      "error",
+    'keyword-spacing': [
+      'error',
       {
-        "before": true,
-        "after": false,
-        "overrides": {
-          "case": {
-            "after": true
+        before: true,
+        after: false,
+        overrides: {
+          case: {
+            after: true
           },
-          "const": {
-            "after": true
+          const: {
+            after: true
           },
-          "else": {
-            "after": true
+          else: {
+            after: true
           },
-          "from": {
-            "after": true
+          from: {
+            after: true
           },
-          "import": {
-            "after": true
+          import: {
+            after: true
           },
-          "return": {
-            "after": true
+          return: {
+            after: true
           }
         }
       }
     ],
-    "linebreak-style": [
-      "error",
-      "unix"
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "new-cap": "error",
-    "no-array-constructor": "error",
-    "no-dupe-class-members": "error",
-    "no-duplicate-imports": "error",
-    "no-nested-ternary": "error",
-    "no-new-object": "error",
-    "no-underscore-dangle": [
-      "error",
+    'new-cap': 'error',
+    'no-array-constructor': 'error',
+    'no-dupe-class-members': 'error',
+    'no-duplicate-imports': 'error',
+    'no-nested-ternary': 'error',
+    'no-new-object': 'error',
+    'no-underscore-dangle': [
+      'error',
       {
-        "allow": [
-          "_layers"
+        allow: [
+          '_layers'
         ],
-        "allowAfterThis": true
+        allowAfterThis: true
       }
     ],
-    "no-unneeded-ternary": "error",
-    "no-useless-call": "error",
-    "no-useless-escape": "error",
-    "no-var": "error",
-    "object-curly-spacing": [
-      "error",
-      "always"
+    'no-unneeded-ternary': 'error',
+    'no-useless-call': 'error',
+    'no-useless-escape': 'error',
+    'no-var': 'error',
+    'object-curly-spacing': [
+      'error',
+      'always'
     ],
-    "object-shorthand": [
-      "error",
-      "properties", {
-        "avoidQuotes": true
+    'object-shorthand': [
+      'error',
+      'properties', {
+        avoidQuotes: true
       }
     ],
-    "one-var": [
-      "error",
-      "always"
+    'one-var': [
+      'error',
+      'always'
     ],
-    "prefer-const": [
-      "error", {
-        "destructuring": "any",
-        "ignoreReadBeforeAssign": false
+    'prefer-const': [
+      'error', {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false
       }
     ],
-    "prefer-template": "error",
-    "quote-props": [
-      "error",
-      "as-needed"
+    'prefer-template': 'error',
+    'quote-props': [
+      'error',
+      'as-needed'
     ],
-    "quotes": [
-      "error",
-      "single"
+    quotes: [
+      'error',
+      'single'
     ],
-    "semi": [
-      "error",
-      "always"
+    semi: [
+      'error',
+      'always'
     ],
-    "space-before-blocks": "error",
-    "space-in-parens": [
-      "error",
-      "never"
+    'space-before-blocks': 'error',
+    'space-in-parens': [
+      'error',
+      'never'
     ],
-    "space-infix-ops": "error",
-    "spaced-comment": [
-      "error",
-      "always", {
-        "block": {
-          "balanced": true
+    'space-infix-ops': 'error',
+    'spaced-comment': [
+      'error',
+      'always', {
+        block: {
+          balanced: true
         }
       }
     ],
-    "vars-on-top": "error",
-    "yoda": "error"
+    'vars-on-top': 'error',
+    yoda: 'error'
   },
   overrides: [
     // node files

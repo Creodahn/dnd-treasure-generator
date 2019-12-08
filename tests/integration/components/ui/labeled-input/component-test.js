065767a13856242701685560f6c790720cf38c1d
Justin Drew <justin.a.drew@gmail.com>
2019-12-07 21:10:37 -0700

    convert to angle brackets

--- tests/integration/components/ui/labeled-input/component-test.js
+++ tests/integration/components/ui/labeled-input/component-test.js
@@ -9,7 +9,7 @@ module('Integration | Component | ui/labeled-input', function(hooks) {
   test('it renders without a label if none is provided', async function(assert) {
     assert.expect(2);
 
-    await render(hbs`{{ui/labeled-input update=(action (mut someValue))}}`);
+    await render(hbs`<Ui::LabeledInput @update={{action (mut someValue)}} />`);
 
     assert.dom('label').doesNotExist();
     assert.dom('input').exists();
@@ -18,7 +18,7 @@ module('Integration | Component | ui/labeled-input', function(hooks) {
   test('it renders with a label if one is provided', async function(assert) {
     assert.expect(2);
 
-    await render(hbs`{{ui/labeled-input label="A label" update=(action (mut someValue))}}`);
+    await render(hbs`<Ui::LabeledInput @label="A label" @update={{action (mut someValue)}} />`);
 
     assert.dom('label').hasText('A label');
     assert.dom('input').exists();
@@ -27,7 +27,7 @@ module('Integration | Component | ui/labeled-input', function(hooks) {
   test('it defaults to a text input if no type is provided', async function(assert) {
     assert.expect(1);
 
-    await render(hbs`{{ui/labeled-input update=(action (mut someValue))}}`);
+    await render(hbs`<Ui::LabeledInput @update={{action (mut someValue)}} />`);
 
     assert.dom('input').hasAttribute('type', 'text');
   });
@@ -41,7 +41,7 @@ module('Integration | Component | ui/labeled-input', function(hooks) {
       assert.equal(value, expected);
     });
 
-    await render(hbs`{{ui/labeled-input input-id="test" update=(action update)}}`);
+    await render(hbs`<Ui::LabeledInput @input-id="test" @update={{action update}} />`);
     await typeIn('[data-test-labeled-input-field="test"]', expected);
   });
 });

1d70c5b6c55eb5faadf8692e4fbe2393049158c8
creodahn <justin@justindrew.net>
2020-07-10 11:32:59 -0600

    dice-bag: abstract out async call for roll event creation to its own function

--- app/services/dice-bag.js
+++ app/services/dice-bag.js
@@ -3,35 +3,12 @@ import Service, { inject as service } from '@ember/service';
 export default Service.extend({
   currentUser: service(),
   store: service(),
+  // methods
+  async _createRollEvent() {
+    const profile = this.currentUser.profile;
 
-  load(dice) {
-    this.set('dice', dice);
-    this.set('rollableDice', dice.filter(die => die.showToUser !== false));
+    return await this.store.createRecord('dice-roll-event', { profile }).save();
   },
-
-  rollDie(dieType, order = 0, diceRollEvent) {
-    const attrs = { order, result: null, diceRollEvent },
-      die = this.dice.findBy('name', dieType);
-
-    if(die) {
-      attrs.die = die;
-      attrs.result = Math.floor(Math.random() * (die.ceil - die.floor + 1)) + die.floor;
-    }
-
-    if(diceRollEvent) {
-      this.store.createRecord('die-roll', attrs).save();
-      // TODO: having to have this feels bad. find a better solution
-    } else if(this.currentUser.profile) {
-      this.store.createRecord('dice-roll-event', { profile: this.currentUser.profile }).save().then((event) => {
-        attrs.diceRollEvent = event;
-
-        this.store.createRecord('die-roll', attrs).save();
-      });
-    }
-
-    return attrs;
-  },
-
   // the `dice` input to this function is mutually exclusive from the `dieType` and `count` inputs
   // `dice` is an explicit list of dice to be rolled, whereas `dieType` and `count` need to be used together
   // to compose a list of dice to roll
@@ -55,12 +32,38 @@ export default Service.extend({
 
     return { rolls, total };
   },
-  async rollMultipleDice(params) {
+  load(dice) {
+    this.set('dice', dice);
+    this.set('rollableDice', dice.filter(die => die.showToUser !== false));
+  },
+  rollDie(dieType, order = 0, diceRollEvent) {
+    const attrs = { order, result: null, diceRollEvent },
+      die = this.dice.findBy('name', dieType);
+
+    if(die) {
+      attrs.die = die;
+      attrs.result = Math.floor(Math.random() * (die.ceil - die.floor + 1)) + die.floor;
+    }
+
+    if(diceRollEvent) {
+      this.store.createRecord('die-roll', attrs).save();
+      // TODO: having to have this feels bad. find a better solution
+    } else if(this.currentUser.profile) {
+      this._createRollEvent().then((event) => {
+        attrs.diceRollEvent = event;
+
+        this.store.createRecord('die-roll', attrs).save();
+      });
+    }
+
+    return attrs;
+  },
+  rollMultipleDice(params) {
     const profile = this.currentUser.profile;
     let rollEvent = null;
 
     if(profile) {
-      rollEvent = await this.store.createRecord('dice-roll-event', { profile }).save();
+      rollEvent = this._createRollEvent();
     }
 
     return this._rollMultipleDice(params, rollEvent);

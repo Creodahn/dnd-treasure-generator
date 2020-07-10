a254d980b702af669c70f33e037709e6f235ea97
creodahn <justin@justindrew.net>
2020-07-10 16:30:27 -0600

    fix linter errors

--- tests/unit/services/rulebook-test.js
+++ tests/unit/services/rulebook-test.js
@@ -6,7 +6,7 @@ module('Unit | Service | rulebook', function(hooks) {
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let service = this.owner.lookup('service:rulebook');
+    const service = this.owner.lookup('service:rulebook');
     assert.ok(service);
   });
 });

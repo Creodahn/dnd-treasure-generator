8139aa224f045708744f6ce085a8d5263745717e
creodahn <justin@justindrew.net>
2020-07-11 10:19:49 -0600

    fix eslint errors in application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 import Route from '@ember/routing/route';
 
+// this `extends` throws a warning, but need to use a mixin due to how ember-simple-auth works
 @classic
 export default class ApplicationRoute extends Route.extend(ApplicationRouteMixin) {
   @service

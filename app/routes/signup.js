8e37e742ad3b42784a630c514c1fc25c8810886a
creodahn <justin@justindrew.net>
2020-07-11 10:20:09 -0600

    fix eslint errors in signup route

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -2,6 +2,7 @@ import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
+// this `extends` throws a warning, but need to use a mixin due to how ember-simple-auth works
 @classic
 export default class SignupRoute extends Route.extend(UnauthenticatedRouteMixin) {
   routeIfAlreadyAuthenticated = 'main';

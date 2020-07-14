6ff6c383ecd7ba1462833ecd2ba002e8e96b4962
creodahn <justin@justindrew.net>
2020-07-14 16:49:36 -0600

    remove classic decorator

--- app/controllers/main/reference/coin-conversions.js
+++ app/controllers/main/reference/coin-conversions.js
@@ -1,20 +1,18 @@
-import classic from 'ember-classic-decorator';
 import Controller from '@ember/controller';
-import Object, { computed }  from '@ember/object';
+import { computed }  from '@ember/object';
 
-@classic
 export default class CoinConversionsController extends Controller {
   @computed('model.[]')
   get coinTableValues() {
     const coins = this.model,
       values = coins.map((coin, index) => {
         return coins.map((coin2, index2) => {
-          return Object.create({
+          return {
             id: `${index}${index2}`.toString(),
             firstCoin: coin.name,
             rate: 1 / (coin.value / coin2.value),
             secondCoin: coin2.name
-          });
+          };
         }).filter((item) => item.rate > 1);
       });
 

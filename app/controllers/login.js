e14963a8080d07aa56c25862ce6a2edc5e366b21
creodahn <justin@justindrew.net>
2020-07-14 15:17:32 -0600

    remove @classic from login controller

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,11 +1,9 @@
-import classic from 'ember-classic-decorator';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 const { error } = console;
 
-@classic
 export default class LoginController extends Controller {
   @service
   currentUser;

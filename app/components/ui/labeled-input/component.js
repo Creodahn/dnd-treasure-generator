2556b338c7738b3d0fc8683c766cb1eaae57b9f8
creodahn <justin@justindrew.net>
2020-07-14 16:24:18 -0600

    update labeled-input to glimmer component

--- app/components/ui/labeled-input/component.js
+++ app/components/ui/labeled-input/component.js
@@ -1,10 +1,5 @@
-import classic from 'ember-classic-decorator';
-import { classNames, classNameBindings } from '@ember-decorators/component';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-@classic
-@classNameBindings('required')
-@classNames('form-group')
 export default class LabeledInput extends Component {
   type = 'text';
 }
